a96442c0e8941e53ac28033a4dc8bef73821d68c
APKNetwork <joshuamaca@gmail.com>
2024-01-15 13:36:22 -0600

    Creating the onboarding pages

--- pages/initapp.py
+++ pages/initapp.py
@@ -26,51 +26,62 @@ class Index(Container):
             expand=True,
             content=Stack(
                 controls=[
-                    Row(
-                        Container(
-                            height=base_height,
-                            width=base_width,
-                            left=10,
-                            top=-200,
-                            content=Image(
-                                src='assets/images/onboarding_4.png',
-                                scale=0.9,
-                                width=100,
-                                height=100,
-                            )
-                        ),
-                        Container(
-                            height=base_height,
-                            width=base_width,
-                            padding=padding.only(top=30,left=10,right=10),
-                            content=Column(
-                                controls=[
-                                    Container(height=20),
-                                    Container(
-                                        margin=margin.only(left=20),
-                                        content=Text(
-                                            value="¡Bienvenido a                          !",
-                                            font_family='Poppins Bold',
-                                            size=20,
-                                        )
-                                    )
-                                ]
-                            )
-                        ),
-                        Container(
-                            height=base_height,
-                            width=base_width,
-                            padding=padding.only(top=0,left=60,right=10),
-                            content=Column(
-                                controls=[
-                                    Container(height=160),
-                                    Container(
-                                        margin=margin.only(left=60)
-                                    )
-                                ]
-                            )
+                    Container(
+                        height=base_height,
+                        width=base_width,
+                        left=10,
+                        top=-200,
+                        content=Image(
+                            src='assets/images/onboarding_4.png',
+                            scale=0.9,
+                            width=100,
+                            height=100,
                         )
                     ),
+                    Container(
+                        height=base_height,
+                        width=base_width,
+                        alignment=alignment.bottom_center,
+                        padding=padding.only(top=30,left=10,right=10),
+                        content=Column(
+                            controls=[
+                                Container(height=20),
+                                Container(
+                                    #border=border.all(4, colors.PINK_600),
+                                    margin=margin.only(left=40),
+                                    content=Text(
+                                        value="¡Bienvenido a                                 !",
+                                        font_family='Poppins Bold',
+                                        text_align=TextAlign.CENTER,
+                                        size=20,
+                                    )
+                                ),
+                                Container(
+                                    #border=border.all(4, colors.BLUE_600),
+                                    margin=margin.only(left= 165,top=-95),
+                                    content=Image(
+                                        src='assets/images/icon_logo.png',
+                                        scale=0.8,
+                                        width=200,
+                                    )
+                                )
+                            ]
+                        )
+                    ),
+                    Container(
+                        height=base_height,
+                        width=base_width,
+                        padding=padding.only(top=0,left=60,right=10),
+                        content=Column(
+                            controls=[
+                                Container(height=160),
+                                Container(
+                                    margin=margin.only(left=60)
+                                )
+                            ]
+                        )
+                    )
+
                 ]
             )
         )        
