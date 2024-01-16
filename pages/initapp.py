975e6b754ea3b0821a8c13ec7dc8d492fb987f9a
APKNetwork <joshuamaca@gmail.com>
2024-01-16 04:24:19 -0600

    Modificando el onboarding pt1

--- pages/initapp.py
+++ pages/initapp.py
@@ -9,7 +9,8 @@ class Index(Container):
 
         page.title = "Welcome to CryDiagnoHealth" #Title Page
         page.fonts = { #fonts
-            "Poppins Bold":"fonts/poppins/Poppins-Bold.ttf"
+            "Poppins Bold":"fonts/poppins/Poppins-Bold.ttf",
+            "Poppins SemiBold":"fonts/poppins/Poppins-SemiBold.ttf"
         }
         #page.add(Image(src=f"/images/logo_wh_cdh_splash.png"))
         row = Row(wrap=True,scroll='always',expand=True)
@@ -17,6 +18,26 @@ class Index(Container):
 
         self.expand = True
         self.offset = transform.Offset(0,0,)
+
+        
+        self.main_content = Column(
+            controls=[
+                Container(
+                    height=btn_height-10,
+                    width=btn_width-150,
+                    border_radius=25,
+                    bgcolor=input_fill_color,
+                    alignment=alignment.center,
+                    content=Text(
+                        value='Siguiente',
+                        font_family='Poppins SemiBold',
+                        size=15,
+                        color=font_color,
+                    )
+                )
+            ]
+        )
+
         self.content = Container(
             height=base_height,
             width=base_width,
@@ -29,11 +50,11 @@ class Index(Container):
                     Container(
                         height=base_height,
                         width=base_width,
-                        left=10,
-                        top=-200,
+                        left=0,
+                        top=-210,
                         content=Image(
-                            src='assets/images/onboarding_4.png',
-                            scale=0.9,
+                            src='assets/images/onboarding_4.png', #Picture one ob
+                            scale=0.8,
                             width=100,
                             height=100,
                         )
@@ -41,12 +62,12 @@ class Index(Container):
                     Container(
                         height=base_height,
                         width=base_width,
-                        alignment=alignment.bottom_center,
+                        alignment=alignment.center,
                         padding=padding.only(top=30,left=10,right=10),
                         content=Column(
                             controls=[
                                 Container(height=20),
-                                Container(
+                                Container( #Text on top boarding
                                     #border=border.all(4, colors.PINK_600),
                                     margin=margin.only(left=40),
                                     content=Text(
@@ -56,33 +77,75 @@ class Index(Container):
                                         size=20,
                                     )
                                 ),
-                                Container(
+                                Container( #onboarding 1
                                     #border=border.all(4, colors.BLUE_600),
-                                    margin=margin.only(left= 165,top=-95),
+                                    margin=margin.only(left= 165,top=-80), #Modificar cuando se pueda cambiar logo sin oso
                                     content=Image(
-                                        src='assets/images/icon_logo.png',
+                                        src='assets/images/logo_whout_bear.png',
                                         scale=0.8,
                                         width=200,
                                     )
-                                )
-                            ]
-                        )
-                    ),
-                    Container(
-                        height=base_height,
-                        width=base_width,
-                        padding=padding.only(top=0,left=60,right=10),
-                        content=Column(
-                            controls=[
-                                Container(height=160),
-                                Container(
-                                    margin=margin.only(left=60)
+                                ),
+                                Container( #Text onboarding
+                                    border=border.all(4, colors.PINK_600),
+                                    height=base_height-600,
+                                    width=base_width,
+                                    padding=padding.only(top=260,left=25,right=25),
+                                    content=Column(
+                                        controls=[
+                                            Container( #Generating lines dashed for scroll
+                                                alignment=alignment.center,
+                                                border=border.all(2, colors.GREEN_600),
+                                                content=onboarding_button,
+                                            ),
+                                            Container(
+                                                alignment=alignment.center,
+                                                height=70,
+                                                border=border.all(4, colors.BLUE_600),
+                                                padding=padding.only(left=50,right=50),
+                                                content=Text(
+                                                    value="Registra llantos de bebés con precisión.",
+                                                    font_family='Poppins SemiBold',
+                                                    text_align=TextAlign.CENTER,
+                                                    size=20,
+                                                    #max_lines=2,
+                                                )
+                                            ),
+                                            Container( #Legend onboard
+                                                alignment=alignment.center,
+                                                margin=margin.only(top=-10),
+                                                content=Text(
+                                                    value="Detecta y graba de manera clara y precisa los sonidos para un análisis efectivo.",
+                                                    font_family='Poppins SemiBold',
+                                                    text_align=TextAlign.CENTER,
+                                                    size=12,
+                                                )
+                                            )
+                                        ]
+                                    )
+                                ),
+                                Container( #Legend onboarding
+                                    padding=20,
+                                    alignment=alignment.center,
+                                    content=self.main_content #going to main content
+
                                 )
                             ]
                         )
                     )
-
                 ]
             )
+        )
+
+onboarding_button = Row(
+    alignment=alignment.center,
+    scroll='auto',
+
+)
+for x in range(3):
+    onboarding_button.controls.append(
+        Container(
+            bgcolor=selected_item, height=5, width=10, border_radius=3, alignment = alignment.center, padding= 15,
         )        
+    )
         #self.bgcolor = deepurple
--- utils/extras.py
+++ utils/extras.py
@@ -5,8 +5,12 @@ btn_width = 350
 btn_height = 50
 br = 30
 bnt_size = 9
-base_color = "#585268"
+#base_color = "#585268" #Deep purple not more than least
+base_color = "#3C3747"
+selected_item = "#6D267B"
+unselected_item = "#ffffff"
 input_fill_color = "#ffffff"
+font_color = "#212121"
 content_padding = padding.only(left=20,top=10,right=10,bottom=10)
 input_error_bg = "#f8c1bc"
 input_error_outline = "#cb1a2a"
